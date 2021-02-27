8b3de762b062775c94cc158a96d301fd662b1a9c
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-02-27 18:14:28 -0300

    templates commit1

--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def data(request):
+    now = datetime.datetime.now()
+    #html = "<html><body>A hora agora é:  %s.</body></html>" % now
+    return render(request, 'contas/data.html')
--- controle_tempo/urls.py
+++ controle_tempo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from contas.views import data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data/', data)
 ]
